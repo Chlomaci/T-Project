a7bd65e93c803026f580687fecab46c4c416e8a5
Darina <darinaeme@gmail.com>
2025-01-25 04:21:30 +0300

    added sorting

--- src/components/ProductsAll.tsx
+++ src/components/ProductsAll.tsx
@@ -1,4 +1,4 @@
-import {FC, useCallback, useContext, useEffect} from "react";
+import {FC, useCallback, useContext, useEffect, useMemo} from "react";
 import './ProductsAll.scss'
 import {useGetProductsQuery} from "../services/fakestore";
 import {Context} from "../pages/MainPage";
@@ -10,15 +10,20 @@ const ProductsAll: FC = () => {
 
 
     const {cartProducts, setCartProducts} = useContext(Context)
-    const { data:products, error, isLoading, refetch } = useGetProductsQuery(10);
+    const { data: products, error, isLoading, refetch } = useGetProductsQuery(10);
 
+    const handleSortProducts = useMemo(() => {
+        if (products) {
+            return [...products].sort((a, b) => a.title.localeCompare(b.title))
+        } return
+
+    }, [products])
 
     useEffect(() => {
       // products ? setCartProducts(products) : null
         if (products) {
             console.log('products effect!')
             setCartProducts(products)
-
         } else {
             console.log('no products')
         }
@@ -35,13 +40,19 @@ const ProductsAll: FC = () => {
         refetch()
     }
 
+    const onSort = () => {
+        const sortedProducts = handleSortProducts;
+        setCartProducts(sortedProducts)
+    }
+
 
 
     return (
         <>
             {isLoading ? 'loading' : error ? 'error' :
                 <section className='productsList'>
-                    <Button onClick={onRefetch} type='primary' style={{marginBottom: '10px'}}>Обновить все!</Button>
+                    <Button onClick={onRefetch} type='primary' style={{marginBottom: '10px', marginRight: '10px'}}>Обновить все!</Button>
+                    <Button onClick={onSort} type='primary' style={{marginBottom: '10px'}}>Отсортируем все! (по наименованию)</Button>
                     <ul className='products__all'>
                         {cartProducts.map(product => {
                             return <ProductCard key={product.id} product={product} handleBuyProduct={handleBuyProduct}/>
